5f2e302f082a11e20a3362356132d48086965eb8
Mshahas786 <mshahas786@gmail.com>
2021-04-09 15:38:00 +1200

    added data freching to home/modal

---
+++ public/modal.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+let titleInput = document.getElementById('title');
+
+let modalInput = document.getElementById('modalId');
+
+titleInput.onchange = handleChange;
+
+function handleChange(e) {
+    modalInput.value= titleInput.value.replace(/\s+/g, '');
+
+}
